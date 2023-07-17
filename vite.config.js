ae1bfdb21e13e1f2505336f470528433c795c6b0
Ayoub RABIE <105998148+Ayoubrabiae@users.noreply.github.com>
2023-07-17 21:58:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import react from "vite-plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-
   build: {
     rollupOptions: {
       external: ["he"],
     },
   },
-};
+});
